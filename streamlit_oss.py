a2ab8cb6fef9a59cd17ae3203a99807195629241
ojiabc <125359690+ojiabc@users.noreply.github.com>
2023-11-15 11:03:13 +0900

    Add files via upload

---
+++ streamlit_oss.py
@@ -0,0 +1,10 @@
+import streamlit as st
+OSS_Score = range(0, 100)
+
+st.write("# OSS test result")
+st.write("## OSS test result")
+st.write("### OSS test result")
+st.write("#### OSS test result")
+st.write("OSS test result")
+OSS_Score
+st.bar_chart(OSS_Score)
