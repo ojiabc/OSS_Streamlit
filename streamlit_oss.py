5ea992987306dbd2f791f5f5f19e2b1210d1fde5
ojiabc <125359690+ojiabc@users.noreply.github.com>
2023-11-15 11:09:12 +0900

    Add files via upload

--- streamlit_oss.py
+++ streamlit_oss.py
@@ -1,7 +1,7 @@
 import streamlit as st
-OSS_Score = range(0, 100)
+OSS_Score = range(-50, 100)
 
-st.write("# 컴퓨터소프트웨어학부 23114563 오재익")
-st.write("## :P")
+st.write("# Hello")
+st.write("## 컴퓨터소프트웨어학부 23114563 오재익")
 OSS_Score
 st.area_chart(OSS_Score)
